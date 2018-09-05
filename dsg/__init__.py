27803c6d7170f7a544153f48cd69fba97e23d94d
RainbowLegend <rainbowlegend3054@gmail.com>
2018-09-05 18:14:44 -0400

    Create __init__.py

---
+++ dsg/__init__.py
@@ -0,0 +1,23 @@
+
+
+from collections import namedtuple
+import logging
+
+__title__ = 'dsg'
+__author__ = 'RainbowLegend'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 RainbowLegend'
+__version__ = '0.1.0'
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='final', serial=0)
+
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+            
+logging.getLogger(__name__).addHandler(NullHandler())
