3f5427cae325e157e5467c349d6c655ebe9db08f
RainbowLegend <rainbowlegend3054@gmail.com>
2018-09-24 19:25:00 -0400

    Update members.py

--- dsg/members.py
+++ dsg/members.py
@@ -1,3 +1,16 @@
+import .errors
+
 class Members:
-    @classmethod
+    
+    def __init__(self, **kwargs):
+        pass
+    
+    @property
+    def isgay(member):
+        if member.lower() == 'rushil':
+            return True
+        elif member.lower() != 'rushil':
+            return False
+        else:
+            raise dsg.dsgException(Exception)
     
