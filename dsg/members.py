b8b0a2111c6945d64ba2c5b06e96bc2ebcad3d99
RainbowLegend <rainbowlegend3054@gmail.com>
2018-09-24 19:17:38 -0400

    Update members.py

--- dsg/members.py
+++ dsg/members.py
@@ -1,18 +1,3 @@
-class Dan:
-    """Represents a member of DSG. Specifically represents Dan."""
-
-    def __init__(self):
-        pass
-
-    @property
-    def github(self):
-        """Returns the github address of the member"""
-
-        return f"https://github.com/RainbowLegend"
-
-    @property
-    def bhsSchedule(self):
-        """Returns the BHS schedule of the member"""
-
-        return "1 - Honors Lit\n2 - PhysEd/HealthEd/Study\n3 - Honors Spanish II\n4 - Honors Bio\n" \
-               "5 - Honors Comp Sci 1\n6 - Honors USH 1\n7 - Honors Geometry"
+class Members:
+    @classmethod
+    
